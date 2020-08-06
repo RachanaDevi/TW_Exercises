0e9e509e6e740fd04da5bb0e42b2e0eaa3353ff0
RachanaDevi <rachanadevi18@gmail.com>
2020-08-06 18:23:41 +0530

    Fixed indentation

--- exercism/Javascript/pig-latin/pig-latin.js
+++ exercism/Javascript/pig-latin/pig-latin.js
@@ -8,37 +8,39 @@ class translator {
     static add_ay_to_word(word){
         if(this.is_two_letter_word(word) && this.has_second_letter_as_y(word)){
 	        return word[1]+word[0]+'ay';
-	}
-
-    else if(this.sounds_like_vowel(word)){
-	    return word+'ay';
-    }
-
-    else if(this.sounds_like_consonant(word)){
-        if(this.consonant_followed_by_qu(word)){
-            let cluster = word.match(this.get_letters_before("qu"));
-            return word.slice(cluster[0].length+2)+cluster[0]+'qu'+'ay';
+	    } 
+    
+        else if(this.sounds_like_vowel(word)){
+	        return word+'ay';
         }
 
-        else if(this.consonant_followed_by_vowel(word)){
-            let cluster =  word.match(this.get_letters_before("[aeiou]"));
-            return word.slice(cluster[0].length)+cluster[0]+'ay';
+        else if(this.sounds_like_consonant(word)){
+            if(this.consonant_followed_by_qu(word)){
+                let cluster = word.match(this.get_letters_before("qu"));
+                return word.slice(cluster[0].length+2)+cluster[0]+'qu'+'ay';
+            }
+
+            else if(this.consonant_followed_by_vowel(word)){
+                let cluster =  word.match(this.get_letters_before("[aeiou]"));
+                return word.slice(cluster[0].length)+cluster[0]+'ay';
+            }
+
+            else if(this.consonant_followed_by_y(word)){
+                let cluster =  word.match(this.get_letters_before("y"));
+                return word.slice(cluster[0].length)+cluster[0]+'ay';
+            }
+
+            else{
+                return word+'ay';
+            }			
         }
 
-        else if(this.consonant_followed_by_y(word)){
-            let cluster =  word.match(this.get_letters_before("y"));
-            return word.slice(cluster[0].length)+cluster[0]+'ay';
-        }
         else{
-            return word+'ay';
-        }			
-    }
+            console.log("Can't be translated!");
+        }
 
-    else{
-        console.log("Can't be translated!");
     }
-
-}
+    
     /* Helper functions */
     static sounds_like_vowel(word){
         return (/^[aeiou]/.test(word) || (/^(xr|yt)/.test(word)));
