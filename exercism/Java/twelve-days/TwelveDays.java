b5294e52689565a8909019a1a5d32c51037dfd1f
RachanaDevi <rachanadevi18@gmail.com>
2020-08-06 17:47:29 +0530

    Added solution for TwelveDays problem statement

---
+++ exercism/Java/twelve-days/TwelveDays.java
@@ -0,0 +1,48 @@
+class TwelveDays {
+	String [] day = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"};
+    String[] gift = {
+            "a Partridge in a Pear Tree.",
+            "two Turtle Doves, and ",
+            "three French Hens, ",
+            "four Calling Birds, ",
+            "five Gold Rings, ",
+            "six Geese-a-Laying, ",
+            "seven Swans-a-Swimming, ",
+            "eight Maids-a-Milking, ",
+            "nine Ladies Dancing, ",
+            "ten Lords-a-Leaping, ",
+            "eleven Pipers Piping, ",
+            "twelve Drummers Drumming, "
+        };
+
+
+    String verse(int verseNumber) {
+    	String giftTillThatDay ="";
+      	for(int i = verseNumber-1; i >= 0; i--){
+    		giftTillThatDay+= gift[i];
+    	}
+
+    	return "On the " + day[verseNumber - 1] + " day of Christmas my true love gave to me: " + giftTillThatDay + "\n";
+
+        
+    }
+
+    String verses(int startVerse, int endVerse) {
+    	String allVerses = "";
+    	for(int i = startVerse; i <= endVerse; i++){
+    		if(i == startVerse){
+    			allVerses+=verse(i);
+
+    		}else{
+    			allVerses+="\n"+verse(i);  		
+    		}
+    		
+    	}
+     return allVerses;
+    }
+    
+    String sing() {
+       return verses(1,12);
+    }
+  
+}
